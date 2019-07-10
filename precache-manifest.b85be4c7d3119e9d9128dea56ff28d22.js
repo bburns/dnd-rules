bfb70cea6f45e4364d1acc8fdbd8170966cae691
Brian Burns <bburns.km@gmail.com>
2019-07-10 05:54:58 -0500

    Updates

---
+++ precache-manifest.b85be4c7d3119e9d9128dea56ff28d22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30b8b7dd85340de54aa4006bae1f2c15",
+    "url": "/dnd-rules/index.html"
+  },
+  {
+    "revision": "39842071bcf54b22d8f6",
+    "url": "/dnd-rules/static/css/main.94b0c379.chunk.css"
+  },
+  {
+    "revision": "7080ecc0723962e19a63",
+    "url": "/dnd-rules/static/js/2.a57095f1.chunk.js"
+  },
+  {
+    "revision": "39842071bcf54b22d8f6",
+    "url": "/dnd-rules/static/js/main.f6ee07f0.chunk.js"
+  },
+  {
+    "revision": "ca09b84d2ed662913833",
+    "url": "/dnd-rules/static/js/runtime~main.ee0ad39f.js"
+  }
+]);
