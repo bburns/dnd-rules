0034dec9d79e5af70492bf43d295074ed05b56fd
Brian Burns <bburns.km@gmail.com>
2019-07-09 20:17:54 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import rules from './assets/rules.json'
 import './assets/f1.css'
 import './assets/f4.css'
@@ -14,20 +14,25 @@ function App() {
   // }, [])
   return (
     <div className="app">
-      <main className="post-content MarkdownBody-wrapified">
-        <div className="h2-sect">
-          <div className="body h3-section-list">
-            {rules.map(rule => (
-              <div className="h3-section">
-                <h3>{rule.Topic}</h3>
-                <div className="body">
-                  {rule.Notes}
+      <div className="body-area">
+        <header class="main-heading -center" role="banner">
+          <h1 class="h1">D&amp;D Rules <em>Cheatsheet</em></h1>
+        </header>
+        <main className="post-content MarkdownBody-wrapified">
+          <div className="h2-sect">
+            <div className="body h3-section-list">
+              {rules.map(rule => (
+                <div className="h3-section">
+                  <h3>{rule.Topic}</h3>
+                  <div className="body">
+                    {rule.Notes}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
+        </main>
         </div>
-      </main>
     </div>
   );
 }
