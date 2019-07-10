585205f24517bf4417d2d741b9e897912696c6dc
Brian Burns <bburns.km@gmail.com>
2019-07-10 05:37:17 -0500

    add build->docs for github pages

---
+++ docs/precache-manifest.758ccc11318607bebc0e8aff8572cdb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa1dc488fdd9ec5506f8e8f70e9f837",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23eb187242804426c17e",
+    "url": "/static/css/main.94b0c379.chunk.css"
+  },
+  {
+    "revision": "7080ecc0723962e19a63",
+    "url": "/static/js/2.a57095f1.chunk.js"
+  },
+  {
+    "revision": "23eb187242804426c17e",
+    "url": "/static/js/main.268be7b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
