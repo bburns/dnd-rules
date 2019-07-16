00d864825d94007427072d0ede689346f651f609
Brian Burns <bburns.km@gmail.com>
2019-07-16 18:15:02 -0500

    org files

--- !open.bat
+++ !open.bat
@@ -1,6 +1,6 @@
 :: open project workspace
 start c:\users\bburns\dropbox\@projects\dnd
 start c:\users\bburns\desktop\workspace\dnd-rules
-code c:\users\bburns\desktop\workspace\dnd-rules
-c:\users\bburns\desktop\workspace\dnd-rules\!start.bat
-c:\users\bburns\desktop\workspace\dnd-rules\!!console.bat
+start code c:\users\bburns\desktop\workspace\dnd-rules
+start c:\users\bburns\desktop\workspace\dnd-rules\!start.bat
+start cmd /k "cd c:\users\bburns\desktop\workspace\dnd-rules && title dndrules"
--- src/packages/neomem/neomem2json.js
+++ src/packages/neomem/neomem2json.js
@@ -1,1 +1,12 @@
 // given raw neomem text, output json
+const fs = require('fs')
+const path = require('path')
+
+
+const s = fs.readFileSync(path.resolve(__dirname, '../../assets/rules.neomem'), 'utf-8')
+console.log(s)
+
+const lines = s.split('\n')
+
+
+
