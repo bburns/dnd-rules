ee49b53e3c4eb17acc2e6f3867baff82d0b88a5c
Brian Burns <bburns.km@gmail.com>
2019-07-16 04:50:57 -0500

    add open .bat

---
+++ !open.bat
@@ -0,0 +1,5 @@
+:: open project workspace
+start c:\users\bburns\dropbox\@projects\dnd
+start c:\users\bburns\desktop\workspace\dnd-rules
+code c:\users\bburns\desktop\workspace\dnd-rules
+c:\users\bburns\desktop\workspace\dnd-rules\!start.bat
