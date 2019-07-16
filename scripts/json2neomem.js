cf38be1918b425b54f04502f92c2ae5b0298f144
Brian Burns <bburns.km@gmail.com>
2019-07-16 05:33:58 -0500

    wip json2neomem

--- scripts/json2neomem.js
+++ scripts/json2neomem.js
@@ -1,7 +1,18 @@
-import fs from 'fs'
-
-console.log("Hello World")
-
 // read json, spit out as formatted neomem text
+//. initially this will also translate csvjson format to tree structure
 
+const fs = require('fs-extra')
 
+// const packageObj = fs.readJsonSync('./package.json')
+// console.log(packageObj.version) 
+
+const rules = fs.readJsonSync('./src/assets/rules.json')
+const line = '---------------------------------------------------------------------------'
+
+for (let rule of rules) {
+  // console.log(rule)
+  console.log(line)
+  console.log(rule.name)
+  console.log(line)
+
+}
