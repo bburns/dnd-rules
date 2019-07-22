35fde9a68894c466b71415349ac0bc84783113b2
Brian Burns <bburns.km@gmail.com>
2019-07-21 22:40:18 -0500

    wip

--- src/packages/neomem/grammar.js
+++ src/packages/neomem/grammar.js
@@ -15,28 +15,39 @@ var grammar = {
     {"name": "main$ebnf$1", "symbols": []},
     {"name": "main$ebnf$1", "symbols": ["main$ebnf$1", "block"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "main", "symbols": ["main$ebnf$1"], "postprocess": d=>`[${d.join(', ')}]`},
-    {"name": "block", "symbols": ["_", "line", "name", "line", "contents"], "postprocess": d=>`{${d[2]}, ${d[4]}}`},
-    {"name": "line$string$1", "symbols": [{"literal":"-"}, {"literal":"-"}, {"literal":"-"}, {"literal":"-"}], "postprocess": function joiner(d) {return d.join('');}},
-    {"name": "line$ebnf$1", "symbols": []},
-    {"name": "line$ebnf$1", "symbols": ["line$ebnf$1", {"literal":"-"}], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "line", "symbols": ["line$string$1", "line$ebnf$1", {"literal":"\n"}], "postprocess": d=>null},
+    {"name": "block", "symbols": ["_", "dashes", "name", "dashes", "contents"], "postprocess": d=>`{${d[2]}, ${d[4]}}`},
+    {"name": "dashes$string$1", "symbols": [{"literal":"-"}, {"literal":"-"}, {"literal":"-"}, {"literal":"-"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "dashes$ebnf$1", "symbols": []},
+    {"name": "dashes$ebnf$1", "symbols": ["dashes$ebnf$1", {"literal":"-"}], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "dashes", "symbols": ["dashes$string$1", "dashes$ebnf$1", {"literal":"\n"}], "postprocess": d=>null},
     {"name": "name$ebnf$1", "symbols": [/[#a-zA-Z0-9'.,!@&(): ]/]},
     {"name": "name$ebnf$1", "symbols": ["name$ebnf$1", /[#a-zA-Z0-9'.,!@&(): ]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "name", "symbols": ["name$ebnf$1", {"literal":"\n"}], "postprocess": d => `"name":"${d[0].join('')}"`},
+    {"name": "char", "symbols": [/./]},
+    {"name": "char", "symbols": [/[\n]/]},
     {"name": "contents$ebnf$1", "symbols": []},
-    {"name": "contents$ebnf$1$subexpression$1", "symbols": [/./]},
-    {"name": "contents$ebnf$1$subexpression$1", "symbols": [/[\n]/]},
-    {"name": "contents$ebnf$1", "symbols": ["contents$ebnf$1", "contents$ebnf$1$subexpression$1"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "contents", "symbols": ["contents$ebnf$1", "line"], "postprocess": d => `"description": "${d[0].join('').trim()}"`},
+    {"name": "contents$ebnf$1", "symbols": ["contents$ebnf$1", "char"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "contents$string$1", "symbols": [{"literal":"-"}, {"literal":"-"}, {"literal":"-"}, {"literal":"-"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "contents", "symbols": ["contents$ebnf$1", "contents$string$1"], "postprocess":  
+        function(d) {
+          const value = d[0]
+          return `"description": "${d[0].join('')}"` 
+        }
+        },
     {"name": "props$ebnf$1", "symbols": []},
     {"name": "props$ebnf$1", "symbols": ["props$ebnf$1", "prop"], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "props", "symbols": ["props$ebnf$1"], "postprocess": d=>d[0].join(', ')},
+    {"name": "props", "symbols": ["props$ebnf$1"], "postprocess": 
+        function(d) {
+          const props = d[0] // list of 0 or more prop json strings
+          return props.join(', ')
+        }
+        },
     {"name": "prop$ebnf$1", "symbols": [/[^:]/]},
     {"name": "prop$ebnf$1", "symbols": ["prop$ebnf$1", /[^:]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "prop$ebnf$2", "symbols": [/./]},
     {"name": "prop$ebnf$2", "symbols": ["prop$ebnf$2", /./], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
-    {"name": "prop", "symbols": ["prop$ebnf$1", /[:]/, "prop$ebnf$2", {"literal":"\n"}], "postprocess": 
-        d => {
+    {"name": "prop", "symbols": ["prop$ebnf$1", /[:]/, "prop$ebnf$2", /[\n]/], "postprocess": 
+        function(d) {
           const name = d[0].join('').trim()
           const value = d[2].join('').trim()
           return `"${name}": "${value}"`
--- src/packages/neomem/grammar.ne
+++ src/packages/neomem/grammar.ne
@@ -1,5 +1,7 @@
 # neomem grammar
 # parse with nearley
+# converts neomem text to json string
+
 
 @builtin "whitespace.ne" # `_` means arbitrary amount of whitespace
 # char -> 
@@ -14,15 +16,15 @@ main -> block:* {% d=>`[${d.join(', ')}]` %}
 # ----------------------------------------------------
 # block
 # ----------------------------------------------------
-# block -> _ line name line props contents {% d=>`{${d[2]}, ${d[4]}, ${d[5]}}` %}
-# block -> _ line name line contents props {% d=>`{${d[2]}, ${d[4]}, ${d[5]}}` %}
-block -> _ line name line contents {% d=>`{${d[2]}, ${d[4]}}` %}
+# block -> _ dashes name dashes props contents {% d=>`{${d[2]}, ${d[4]}, ${d[5]}}` %}
+# block -> _ dashes name dashes contents props {% d=>`{${d[2]}, ${d[4]}, ${d[5]}}` %}
+block -> _ dashes name dashes contents {% d=>`{${d[2]}, ${d[4]}}` %}
 
 
 # ----------------------------------------------------
-# line
+# dashes
 # ----------------------------------------------------
-line -> "----" "-":* "\n"   {% d=>null %}
+dashes -> "----" "-":* "\n"   {% d=>null %}
 
 
 # ----------------------------------------------------
@@ -34,24 +36,47 @@ name -> [#a-zA-Z0-9'.,!@&(): ]:+ "\n"   {% d => `"name":"${d[0].join('')}"` %}
 # ----------------------------------------------------
 # contents
 # ----------------------------------------------------
-contents -> (. | [\n]):* line  {% d => `"description": "${d[0].join('').trim()}"` %}
+# contents -> (. | [\n]):* line  {% d => `"description": "${d[0].join('').trim()}"` %}
+# contents -> (.:* [\n]):* dashes
+
+# paragraph -> .:* [\n]   {% null %}
+# contents -> paragraph:* dashes
+
+char -> . | [\n]
+contents -> char:* "----"
+
+{% 
+  function(d) {
+    const value = d[0]
+    return `"description": "${d[0].join('').trim()}"` 
+  }
+%}
 
 
 # ----------------------------------------------------
 # props
 # ----------------------------------------------------
-props -> prop:* {% d=>d[0].join(', ') %}
+# props -> prop:* {% d=>d[0].join(', ') %}
+
+props -> prop:*
+
+{%
+  function(d) {
+    const props = d[0] // list of 0 or more prop json strings
+    return props.join(', ')
+  }
+%}
 
 
 # ----------------------------------------------------
 # prop
 # ----------------------------------------------------
-prop -> [^:]:+ [:] .:+ "\n"
+# prop -> [^:]:+ [:] .:+ "\n"  {% d => `"${d[0].join('').trim()}":"${d[2].join('').trim()}"` %}
 
-# d => `"${d[0].join('').trim()}":"${d[2].join('').trim()}"` 
+prop -> [^:]:+  [:]  .:+ [\n]
 
 {%
-  d => {
+  function(d) {
     const name = d[0].join('').trim()
     const value = d[2].join('').trim()
     return `"${name}": "${value}"`
