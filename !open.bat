195e84b1e708b5897a6b1d425c1af781a982f1b6
Brian Burns <bburns.km@gmail.com>
2019-07-22 19:51:04 -0500

    bat

--- !open.bat
+++ !open.bat
@@ -1,6 +1,6 @@
 :: open project workspace
-start c:\users\bburns\dropbox\@projects\dnd
-start c:\users\bburns\desktop\workspace\dnd-rules
+start /max c:\users\bburns\dropbox\@projects\dnd
+start /max c:\users\bburns\desktop\workspace\dnd-rules
 start code c:\users\bburns\desktop\workspace\dnd-rules
-start c:\users\bburns\desktop\workspace\dnd-rules\!start.bat
-start cmd /k "cd c:\users\bburns\desktop\workspace\dnd-rules && title cmd"
+start /max cmd /k "title start && yarn start"
+start /max cmd /k "title cmd && cd c:\users\bburns\desktop\workspace\dnd-rules"
