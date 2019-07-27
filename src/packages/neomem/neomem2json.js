6e62fd5784d46a919a5f5b2dbc9a3b66671ba988
Brian Burns <bburns.km@gmail.com>
2019-07-27 02:02:25 -0500

    wip

--- src/packages/neomem/neomem2json.js
+++ src/packages/neomem/neomem2json.js
@@ -1,12 +1,34 @@
 // given raw neomem text, output json
+
 const fs = require('fs')
-const path = require('path')
+// const path = require('path')
 
+// const s = fs.readFileSync(path.resolve(__dirname, '../../assets/rules.neomem'), 'utf-8')
+const s = fs.readFileSync(0).toString()
 
-const s = fs.readFileSync(path.resolve(__dirname, '../../assets/rules.neomem'), 'utf-8')
-console.log(s)
 
 const lines = s.split('\n')
+
+// const states = {}
+
+let state = 0
+
+// lines.forEach(line => {
+//   console.log(line)
+// })
+
+for (let i = 0; i < lines.length; i++) {
+  const line = lines[i];
+  const lineType = getLineType(line)
+  console.log(line, lineType)
+}
+
+
+// get type of line based on regexp and current state
+function getLineType(line, state) {
+  const lineType = line.match(/foo/)
+  console.log(lineType)
+}
 
 
 
