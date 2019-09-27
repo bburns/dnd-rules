f8465ad17198bc7ec3f076d78f13ba788dec45c9
Brian Burns <bburns.km@gmail.com>
2019-09-27 03:50:26 -0500

    words

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -50,7 +50,7 @@ export default function() {
           <div className="app-intro">
             <h2>Introduction</h2>
             <div>
-              This is a summary of the D&amp;D (5th edition) rules, split into three sections - Basic, Intermediate, and Advanced. You can start by playing with the Basic rules and add more as needed. 
+              This is an unofficial summary of the D&amp;D (5th edition) rules, split into three sections - Basic, Intermediate, and Advanced. You can start by playing with the Basic rules and add more as needed. 
             </div>
             <br/>
             <div>
